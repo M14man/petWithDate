f5ba17dfa81ead5a736d86babbaa24af2f96b653
Dmyto_Turovskiy <turovskydo@gmail.com>
2022-04-22 18:26:14 +0300

    Starting a project

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+
+import form from './modules/form';
+import language from './modules/changeLanguage';
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    
+    let state = {
+    };
+
+    form();
+    language();
+
+});
+
